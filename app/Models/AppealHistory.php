88a357fdbdcc2087a7886bc752f95fcfd0fa7ad6
Kozy-Korpesh Tolep <kozykorpesh.tolep@nu.edu.kz>
2023-08-28 13:34:36 +0600

    appeals

---
+++ app/Models/AppealHistory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AppealHistory extends Model
+{
+    protected $table = "appeal_history";
+
+    const STATUS_DONE = "Выполнено";
+    const STATUS_SENT = "Отправлено";
+    const STATUS_DRAFT = "Черновик";
+
+    protected $modelName = __CLASS__;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'link',
+        'status',
+        'reply',
+        'user_id',
+    ];
+}
