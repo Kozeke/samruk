060e497d974b05b4f6ca3fd8418735763af62317
Kozy-Korpesh Tolep <kozykorpesh.tolep@nu.edu.kz>
2023-09-06 14:29:00 +0600

    appeals

--- app/Http/Controllers/Site/Cabinet/CabinetController.php
+++ app/Http/Controllers/Site/Cabinet/CabinetController.php
@@ -1454,7 +1454,7 @@ class CabinetController extends BaseController
         $pdf = new Dompdf($options);
         $html = $this->getHeaderHtml($data);
         $html .= $this->getContentHtmlForCodeId($data, $request, $request['selected_code_id']);
-//        $html .= $this->getFooterHtml($request);
+        $html .= $this->getFooterHtml($request);
 
         $pdf->load_html($html, 'UTF-8');
         $pdf->render();
