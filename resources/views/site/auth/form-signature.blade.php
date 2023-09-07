fbf0a30d8bd84b0bf81245480e536e31877cd9c1
Kozy-Korpesh Tolep <kozykorpesh.tolep@nu.edu.kz>
2023-09-07 22:12:12 +0600

    appeals

--- resources/views/site/auth/form-signature.blade.php
+++ resources/views/site/auth/form-signature.blade.php
@@ -1,7 +1,7 @@
 <div class="form-auth__signature">
     <div class="form-auth__signature-left" >
         <input type="hidden" name="PKCS12" value="PKCS12" id="storageSelect">
-        <a class="form-auth__signature-choose" href="javascript:;" onclick="signConsentToDataCollection()">
+        <a class="form-auth__signature-choose" href="javascript:;" onclick="createCAdESFromBase64BackForConsent()">
             {!! icon('icon--key') !!}
             <span>Выбрать ЭЦП</span>
         </a>
