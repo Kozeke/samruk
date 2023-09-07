9854e6af8edabe0637b80aa723abc12697ac5682
Kozy-Korpesh Tolep <kozykorpesh.tolep@nu.edu.kz>
2023-09-07 12:22:45 +0600

    appeals

--- app/Http/Controllers/Site/Cabinet/CabinetController.php
+++ app/Http/Controllers/Site/Cabinet/CabinetController.php
@@ -1759,7 +1759,10 @@ HTML;
         if ($request['signed']) {
             $this->verifyData($request['cms_pdf'], $request['base_pdf']);
             $html .= "<div style='right:0px;position:absolute;'>";
-            $html .= DNS2D::getBarcodeHTML("asd", 'QRCODE', 5, 5);
+            $html .= DNS2D::getBarcodeHTML($request['cms_pdf'], 'QRCODE', 5, 5);
+            $html .= " <p style='font-size: 10px'>Осы құжат \"Электрондық құжат және электрондық цифрлық қолтаңба туралы\" Қазақстан Республикасының 2003 жылғы 7
+            қаңтардағы N 370-II Заңы 7 бабының 1 тармағына сәйкес қағаз тасығыштағы құжатпен бірдей.
+            Данный документ согласно пункту 1 статьи 7 ЗРК от 7 января 2003 года N370-II Об электронном документе и электронной цифровой подписи\" равнозначен документу на бумажном носителе.</p>";
             $html .= "</div></div>";
         } else {
             $html .= "<p style='text-align: right;'>Подпись:</p>
@@ -1972,6 +1975,7 @@ HTML;
                 echo "Error: " . $err . "\n";
             }
         } else {
+            dd($outData);
             $this->FIOCertificateOwner .= str_replace("CN=", "", $outData);
         }
 
