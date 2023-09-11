2b988bfdefafff652ef4934627c93bcb44a3ae9b
Kozy-Korpesh Tolep <kozykorpesh.tolep@nu.edu.kz>
2023-09-11 19:51:16 +0600

    appeals

--- app/Services/AppealService.php
+++ app/Services/AppealService.php
@@ -440,8 +440,9 @@ HTML;
             $html = "<p style='text-align: right;'>{$today_date}</p>";
             if ($request['signed']) {
                 $html .= "<div style='right:0px;position: absolute;'>";
-                $html .= DNS2D::getBarcodeHTML($this->signerInfo?:"", 'QRCODE', 5, 5) . "</div>";
-                $html .= DNS2D::getBarcodeHTML($data['number'], 'QRCODE', 5, 5) . "</div>";
+                $html .= DNS2D::getBarcodeHTML($this->signerInfo, 'QRCODE', 5, 5) . "</div>";
+                $html .= "<div style='right:200px;position: absolute;'>";
+                $html .= DNS2D::getBarcodeHTML("Номер договора №".$data['number'], 'QRCODE', 5, 5) . "</div>";
                 $html .= "<div style='bottom: 0px;position: absolute'> <p style='font-size: 10px'>
             Данный документ согласно пункту 1 статьи 7 ЗРК от 7 января 2003 года N370-II Об электронном документе и электронной цифровой подписи\" равнозначен документу на бумажном носителе.</p>";
                 $html .= "</div></div>";
