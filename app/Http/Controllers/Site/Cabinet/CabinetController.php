4760c31eaf572b8e2c0c1f1fdfe1b0e18d5ccb44
Kozy-Korpesh Tolep <kozykorpesh.tolep@nu.edu.kz>
2023-12-22 10:32:08 +0100

    appeals

--- app/Http/Controllers/Site/Cabinet/CabinetController.php
+++ app/Http/Controllers/Site/Cabinet/CabinetController.php
@@ -95,32 +95,32 @@
                     "num_phone" => '+7 (701) 629 40 26',
                     "date_zp" => Carbon::now()->format('YmdHis')
                 ])->_toArray();
-                $this->info = [
-                    "code" => 200,
-                    "data" => [
-                        "result" => "0",
-                        "comment" => "Ok!",
-                        "FIO" => "Садубаев Адилбек",
-                        "mail" => [],
-                        "Num_d" => [
-                            "number" => "301200241",
-                            "date_d" => "15.06.2023 0:00:00",
-                            "JK" => "ЖК по улице Улы Дала, дом 17",
-                            "AdressJK" => "Проспект Улы Дала 17",
-                            "Korpus" => [],
-                            "Dom" => "_",
-                            "Number_room" => "Квартира №32, дом №17/1, этаж 7, 66,7 кв.м.",
-                            "gar_plat_by_dog" => [],
-                            "sum_d" => "1 166 667",
-                            "plat_d" => "0",
-                            "plat_date" => [],
-                            "zad_plat" => "0",
-                            "penya" => "0",
-                            "gar_plat" => "0",
-                            "im_nalog" => "0",
-                        ]
-                    ]
-                ];
+//                $this->info = [
+//                    "code" => 200,
+//                    "data" => [
+//                        "result" => "0",
+//                        "comment" => "Ok!",
+//                        "FIO" => "Садубаев Адилбек",
+//                        "mail" => [],
+//                        "Num_d" => [
+//                            "number" => "301200241",
+//                            "date_d" => "15.06.2023 0:00:00",
+//                            "JK" => "ЖК по улице Улы Дала, дом 17",k
+//                            "AdressJK" => "Проспект Улы Дала 17",
+//                            "Korpus" => [],
+//                            "Dom" => "_",
+//                            "Number_room" => "Квартира №32, дом №17/1, этаж 7, 66,7 кв.м.",
+//                            "gar_plat_by_dog" => [],
+//                            "sum_d" => "1 166 667",
+//                            "plat_d" => "0",
+//                            "plat_date" => [],
+//                            "zad_plat" => "0",
+//                            "penya" => "0",
+//                            "gar_plat" => "0",
+//                            "im_nalog" => "0",
+//                        ]
+//                    ]
+//                ];
                 if ($this->info['code'] == 200) {
                     $this->data = $this->info['data'];
                     if ($this->data['result'] == 1) {
