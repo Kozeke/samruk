ceca683381ccb114cda5448a2ca84c2dcd4daa73
Kozy-Korpesh Tolep <kozykorpesh.tolep@nu.edu.kz>
2023-12-27 17:32:08 +0100

    appeals

--- app/Services/AppealService.php
+++ app/Services/AppealService.php
@@ -443,7 +443,10 @@ HTML;
                 if ($this->signerInfo) {
                     $arr = explode(",", $this->signerInfo);
                     $html .= "<div style='right:0px;position: absolute;'>";
-                    $html .= DNS2D::getBarcodeHTML("Куралбаева Ажаргуль 740229399404", 'QRCODE', 3, 3) . "</div>";
+                    $html .= "<img src='data:image/png;base64," . DNS2D::getBarcodePNG(
+                            $arr[1].'#'. $arr[0],
+                            'QRCODE'
+                        ) . "alt='barcode' /></div>";
                     $html .= "<div style='bottom: 0px;position: absolute'> <p style='font-size: 10px'>
             Данный документ согласно пункту 1 статьи 7 ЗРК от 7 января 2003 года N370-II \"Об электронном документе и электронной цифровой подписи\" равнозначен документу на бумажном носителе.</p>";
                     $html .= "</div></div>";
